2776793bb50fed160f3c69b8963508521f15238b
Guilherme Andre <gui1andre89@gmail.com>
2024-04-02 22:09:58 -0300

    desafio

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+var nokia = new Nokia("119843718347", "Tijolo", "245432523452345", 64);
+
+var iphone = new Iphone("11948395738", "15 Pro", "2342352353452345", 256);
+
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("whatsapp");
+
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("instagram");
